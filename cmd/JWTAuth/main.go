96445a85f55d7e94cbce5e7db7c89b642b9c5b8c
ShlykovPavel <pavel14640@gmail.com>
2025-04-29 19:44:23 +0300

    add info for setup logger func

--- cmd/JWTAuth/main.go
+++ cmd/JWTAuth/main.go
@@ -20,6 +20,27 @@ func main() {
 	log.Debug("Debug messages enabled")
 }
 
+// setupLogger
+//
+// Configures and initializes a structured logger (slog.Logger) tailored to the specified runtime environment.
+// The logger outputs log messages in JSON format, ensuring consistency and ease of parsing across different environments.
+//
+// Parameters:
+// - env (string): The runtime environment. Supported values:
+//   - envLocal: Local development environment. Enables Debug-level logging for detailed debugging.
+//   - envDev: Shared development or staging environment. Also enables Debug-level logging.
+//   - envProd: Production environment. Restricts logging to Info level to reduce noise and focus on critical events.
+//
+// Behavior:
+//   - In local (`envLocal`) and development (`envDev`) environments, the logger is configured with the `Debug` level.
+//     This ensures that all log messages, including debug-level information, are captured and output in JSON format.
+//     This is particularly useful for troubleshooting and detailed analysis during development.
+//   - In the production environment (`envProd`), the logger is configured with the `Info` level. This ensures that only
+//     informational and higher-priority logs (e.g., warnings, errors) are captured, reducing verbosity and focusing on
+//     critical operational data.
+//
+// Returns:
+// - *slog.Logger: A configured logger instance ready for use in the specified environment.
 func setupLogger(env string) *slog.Logger {
 	var log *slog.Logger
 	switch env {
